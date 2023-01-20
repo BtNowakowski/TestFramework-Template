9d583a27ec57e63dd467d49efa6edbd5bf77bf76
BtNowakowski <107316656+BtNowakowski@users.noreply.github.com>
2023-01-20 15:59:15 +0100

    *

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -3,11 +3,12 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+
 # from selenium.webdriver.firefox.options import Options as FirefoxOptions
 # from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 
 from Base.base_driver import BaseDriver
 
@@ -18,7 +19,6 @@ def setup(request):
     options = ChromeOptions()
     options.page_load_strategy = 'normal'
     driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-    wait = WebDriverWait(driver, 10)
 
     driver.get("https://www.pracuj.pl/")
     driver.maximize_window()
@@ -30,9 +30,10 @@ def setup(request):
         bd.wait_for_clickable_element(By.XPATH, '//*[@id="__next"]/div/div[6]/div/div/div/div[4]/div/button[2]').click()
         bd.wait_for_clickable_element(By.XPATH, '//*[@data-test="button-submit"]').click()
         # wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="popupContainer"]/div/div/div[1]/button'))).click()
-        # wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/div/div[6]/div/div/div/div[4]/div/button[2]'))).click()
+        # wait.until(EC.element_to_be_clickable((By.XPATH,
+        # '//*[@id="__next"]/div/div[6]/div/div/div/div[4]/div/button[2]'))).click()
         # wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@data-test="button-submit"]'))).click()
-    except Exception:
+    except BaseException:
         pass
 
     # Teardown, after test is done
