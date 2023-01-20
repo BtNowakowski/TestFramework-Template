e32b163bd86336e79a5660f262cf07da6cdbb9fa
BtNowakowski <107316656+BtNowakowski@users.noreply.github.com>
2023-01-20 15:54:17 +0100

    *

--- Pages/pracujpl_launch_page.py
+++ Pages/pracujpl_launch_page.py
@@ -1,8 +1,6 @@
-import time
-
+# import time
+# from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-
 from Base.base_driver import BaseDriver
 
 
--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -3,11 +3,13 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.firefox.service import Service as FirefoxService
+# from selenium.webdriver.firefox.options import Options as FirefoxOptions
+# from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+
+from Base.base_driver import BaseDriver
 
 
 @pytest.fixture(scope="class")
@@ -23,11 +25,14 @@ def setup(request):
     request.cls.driver = driver
 
     try:
-        # Cookies decline
-        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="popupContainer"]/div/div/div[1]/button'))).click()
-        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/div/div[6]/div/div/div/div[4]/div/button[2]'))).click()
-        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@data-test="button-submit"]'))).click()
-    finally:
+        bd = BaseDriver(driver)
+        bd.wait_for_clickable_element(By.XPATH, '//*[@id="popupContainer"]/div/div/div[1]/button').click()
+        bd.wait_for_clickable_element(By.XPATH, '//*[@id="__next"]/div/div[6]/div/div/div/div[4]/div/button[2]').click()
+        bd.wait_for_clickable_element(By.XPATH, '//*[@data-test="button-submit"]').click()
+        # wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="popupContainer"]/div/div/div[1]/button'))).click()
+        # wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/div/div[6]/div/div/div/div[4]/div/button[2]'))).click()
+        # wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@data-test="button-submit"]'))).click()
+    except Exception:
         pass
 
     # Teardown, after test is done
